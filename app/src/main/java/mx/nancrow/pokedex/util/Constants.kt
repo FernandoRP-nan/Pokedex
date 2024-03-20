40bc3cba34aad729cdddb75aa9b073adecfbcd46
fernando.r <fernando.r.satoritech@gmail.com>
2024-03-20 14:29:43 -0600

    fix(general) - data correct

--- app/src/main/java/mx/nancrow/pokedex/util/Constants.kt
+++
@@ -1,6 +0,0 @@
-package mx.nancrow.pokedex.util
-
-object Constants {
-    const val EMPTY_STRING = ""
-    const val SHARE_EVENT_URL = "https://fest-lovers-web.dev.satoritech.com.mx/client/event/"
-}
